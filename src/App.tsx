9ccb20ebda466c1237eeb8e4078c1ef0d8dc3718
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-02-04 17:01:41 -0600

    Add fetch call and dynamic content based off state status

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import UsersList from './features/users'
+import { AppDispatch, RootState } from './app/store/store'
+import { useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { getRestaurants } from './features/restaurants/restaurantsSlice'
 
 function App() {
   const dispatch = useDispatch<AppDispatch>()
+  const { status, restaurants, error } = useSelector((state: RootState) => state.restaurants)
 
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(getRestaurants())
+    }
+  }, [status, dispatch])
+
+  let content
+  if (status === 'loading') {
+    content = <p>Loading...</p>
+  } else if (status === 'succeeded') {
+    const renderedRestaurants = restaurants.map((restaurant) => (
+      <div>
+        <p>{restaurant.name}</p>
+        <img style={{ width: 350 }} src={restaurant.coverImg} />
+      </div>
+    ))
+    content = (
+      <div>
+        <h2>List of Restaurants</h2>
+        {renderedRestaurants}
+      </div>
+    )
+  } else if (status === 'failed') {
+    content = (
+      <div>
+        <h2 style={{ color: 'red' }}>ERROR</h2>
+        <p>{error}</p>
+      </div>
+    )
+  }
 
   return (
     <div>
       <UsersList />
+      {content}
     </div>
   )
 }
