be82e3e0e82a876e10b8cc62552df204324bb999
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-02-04 18:07:49 -0600

    Update catch to return error message

--- src/utilities/APICalls.ts
+++ src/utilities/APICalls.ts
@@ -1,18 +1,16 @@
-import { API_ROUTES } from "./constants"
+import { API_ROUTES } from './constants'
 
 export const fetchRestaurants = async () => {
- try {
-  const response = await fetch(API_ROUTES.GET_RESTAURANTS, {
+  try {
+    const response = await fetch(API_ROUTES.GET_RESTAURANTS, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
       },
-  })
+    })
 
-  return await response.json()
-
- } catch (err) {
-  console.error
- }
+    return await response.json()
+  } catch (error) {
+    return error
+  }
 }
-
