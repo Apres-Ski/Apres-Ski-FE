8a4fb483d3b416679e5b6477b31917bdd016fcfd
Matt Walter <matt.walter.tx@outlook.com>
2023-02-06 20:59:47 -0600

    Adjust Sorry comp to add reset filter options btn

--- src/components/Sorry/Sorry.tsx
+++ src/components/Sorry/Sorry.tsx
@@ -5,8 +5,9 @@ export const Sorry = () => {
       <h2>Sorry Component (Filter)</h2>
       <p>No matches for this criteria. Please refine your selections and try again.</p>
       <button>Change Options</button>
+      <button>Reset Filters</button>
     </div>
   )
 }
 
-// Route button to open Filter menu
+// Route buttons to open Filter menu w/ selected option
