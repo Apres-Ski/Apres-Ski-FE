57056ea936d62a36ad0698ba2379e2ff192474f3
Matt Walter <matt.walter.tx@outlook.com>
2023-02-07 17:30:20 -0600

    Setup route Landing > Demo

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,10 +1,14 @@
+import { Link } from 'react-router-dom'
+
 export const Landing = () => {
 
   return (
     <div>
       <h2>Landing Component</h2>
       <p>Placeholder - Welcome to our Application demo</p>
-      <button>Start Demo</button>
+      <Link to='/demo'>
+        <button>Start Demo</button>
+      </Link>
     </div>
   )
 }
