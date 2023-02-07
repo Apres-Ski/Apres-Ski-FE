0a2cd832232c62b761646d1331b1782102e91066
Matt Walter <matt.walter.tx@outlook.com>
2023-02-07 17:32:01 -0600

    Setup route Error > App

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,10 +1,14 @@
+import { Link } from 'react-router-dom'
+
 export const Error = () => {
 
   return (
     <div>
       <h2>Error Component (General)</h2>
       <p>There seems to be a problem loading this content. Please return to the Main page</p>
-      <button>Go Back</button>  
+      <Link to='/'>
+        <button>Go Back</button>  
+      </Link>
     </div>
   )
 }
