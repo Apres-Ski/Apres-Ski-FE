b2396b37e727521f43e354623b793f172b64b4c6
Ryan Nagel <RNagel29@gmail.com>
2023-02-08 15:04:24 -0700

    Add failed assertion for testing purposes

--- cypress/e2e/landing.cy.ts
+++ cypress/e2e/landing.cy.ts
@@ -2,6 +2,10 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
+
+  it('should fail this test', () => {
+    expect(true).to.equal(false)
+  })
 })
 
 export {} //I'm not sure why this was required
