3dce21f7f4eb6eab93d0315249d0178be0d560d5
Ryan Nagel <RNagel29@gmail.com>
2023-02-08 19:16:51 -0700

    Update data cleaning in utilities

--- src/utilities/interfaces.ts
+++ src/utilities/interfaces.ts
@@ -1,35 +1,35 @@
 export interface Hours {
-    "id": number
-    "sunday": string
-    "monday": string
-    "tuesday": string
-    "wednesday": string
-    "thursday": string
-    "friday": string
-    "saturday": string
-    "restaurant": number
+    id: number
+    sunday: string
+    monday: string
+    tuesday: string
+    wednesday: string
+    thursday: string
+    friday: string
+    saturday: string
+    restaurant: number
 }
 
 export interface HappyHours {
-  "id": number
-  "sunday": string
-  "monday": string
-  "tuesday": string
-  "wednesday": string
-  "thursday": string
-  "friday": string
-  "saturday": string
-  "special": string
-  "restaurant": number
+  id: number
+  sunday: string
+  monday: string
+  tuesday: string
+  wednesday: string
+  thursday: string
+  friday: string
+  saturday: string
+  special: string
+  restaurant: number
 }
 
 export interface Engagement {
-    "id": number
-    "vibe": string
-    "rating": string
-    "favorites": boolean
-    "restaurant": number[]
-    "user": number[]
+    id: number
+    vibe: string
+    rating: string
+    favorites: boolean
+    restaurant: number[]
+    user: number[]
 }
 
 export interface RestaurantsState {
@@ -44,25 +44,33 @@ export interface RestaurantsState {
     alt_text: string
     lat: string
     lon: string
-    alcoholic_drinks: string // fix spelling
-    has_happy_hour: string
+    alcoholic_drinks: boolean
+    has_happy_hour: boolean
     hour: Hours[]
-    happyhour?: HappyHours[]
+    happyhour: HappyHours[]
     engagement: Engagement[]
   }
 }
 
 export interface CleanedHours {
-  id: string
-  weekday: string
-  hours: string
+  sunday: string
+  monday: string
+  tuesday: string
+  wednesday: string
+  thursday: string
+  friday: string
+  saturday: string
 }
 
 export interface CleanedHappyHours {
-  id: string
-  weekday: string
-  hours: string
-  specials: string
+  sunday: string
+  monday: string
+  tuesday: string
+  wednesday: string
+  thursday: string
+  friday: string
+  saturday: string
+  special: string
 }
 
 export interface CleanedRestaurantsState {
@@ -71,15 +79,17 @@ export interface CleanedRestaurantsState {
   address: string
   foodType: string
   cost: string
-  vibe: string
+  vibes: string[] | undefined
   coverImg: string
   alt: string
   location: {
     lat: string
     long: string
   }
-  alcoholic: string
-  happyHour: string
-  hours: CleanedHours[]
-  happyHours: CleanedHappyHours[]
+  alcoholic: boolean
+  happyHour: boolean
+  hours: CleanedHours
+  happyHours: CleanedHappyHours
+  engagements: Engagement[]
+  avgRating: number
 }
