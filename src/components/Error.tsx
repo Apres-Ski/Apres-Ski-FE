4a0b4e5b4725ddcb2cfd50f8dd439eae649e4ef1
Ryan Nagel <RNagel29@gmail.com>
2023-02-10 14:58:07 -0700

    Add react modal component to Error

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,16 +1,34 @@
 import { Link } from 'react-router-dom'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 export const Error = () => {
 
   return (
-    <div>
-      <h2>Error Component (General)</h2>
-      <p>There seems to be a problem loading this content. Please return to the Main page</p>
-      <Link to='/'>
-        <button>Go Back</button>  
-      </Link>
-    </div>
-  )
+      <div
+        className="modal show"
+        style={{ display: 'block', position: 'initial' }}
+      >
+        <Modal.Dialog>
+            <Modal.Title>Error</Modal.Title>
+  
+          <Modal.Body>
+            <p>404 Page Not Found</p>
+          </Modal.Body>
+  
+          <Modal.Footer className='d-flex justify-content-center'>
+            <Link to='/'><Button variant='primary'>Close</Button></Link>
+          </Modal.Footer>
+        </Modal.Dialog>
+      </div>
+    )
+    // <div>
+    //   <h2>Error Component (General)</h2>
+    //   <p>There seems to be a problem loading this content. Please return to the Main page</p>
+    //   <Link to='/'>
+    //     <button>Go Back</button>  
+    //   </Link>
+    // </div>
 }
 
 // Route button to Main page
