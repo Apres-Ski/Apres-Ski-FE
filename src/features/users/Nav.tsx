659085e2b2cdb3613a534499a60947d3ddaca1f9
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-02-10 09:06:12 -0600

    Add React Router Bootstrap

--- src/features/users/Nav.tsx
+++ src/features/users/Nav.tsx
@@ -3,41 +3,35 @@ import { RootState } from '../../app/store'
 import { Link } from 'react-router-dom'
 import { resetUser } from './usersSlice'
 import Navbar from 'react-bootstrap/Navbar'
-import { Container, Nav, NavDropdown } from 'react-bootstrap'
+import { Button, Container, Nav, NavDropdown } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import { APP_ROUTES } from '../../utilities/constants'
 
 export const NavMenu = () => {
   const { activeUser } = useSelector((state: RootState) => state.users)
-
   const dispatch = useDispatch()
 
-  const userName = activeUser.name.split(' ')
-  let grabName = userName[0]
-
   return (
-    <Navbar bg="light" expand="lg" fixed='top'>
+    <Navbar bg="primary" variant="dark" expand="lg" fixed="top">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <LinkContainer to={APP_ROUTES.LANDING}>
+          <Navbar.Brand href="#home">Après Ski ⛷</Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as: <a href="#login">{activeUser.name}</a>
+            <Button
+              variant="outline-success"
+              onClick={() => dispatch(resetUser(activeUser))}
+            >
+              Logout
+            </Button>
+          </Navbar.Text>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  );
+  )
 }
 // <div>
 //   <h2>Nav Component</h2>
