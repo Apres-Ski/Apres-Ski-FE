c9a26d670b35021af1d3bc165fe4c008dc884777
Ryan Nagel <RNagel29@gmail.com>
2023-02-10 15:47:11 -0700

    change component to look more like an actual modal

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,26 +1,30 @@
 import { Link } from 'react-router-dom'
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+import { useState } from 'react'
 
 export const Error = ({ message }: { message: string}) => {
+  const [show, setShow] = useState(true);
+
+  const handleClose = () => setShow(false);
 
   return (
-      <div
-        className="modal show"
-        style={{ display: 'block', position: 'initial' }}
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
       >
-        <Modal.Dialog>
+        <Modal.Header className='d-flex justify-content-center'>
             <Modal.Title className='text-danger'>Error</Modal.Title>
-  
-          <Modal.Body>
+        </Modal.Header>
+          <Modal.Body className='d-flex justify-content-center'>
             <p>{message}</p>
           </Modal.Body>
   
           <Modal.Footer className='d-flex justify-content-center'>
             <Link to='/'><Button variant='primary'>Close</Button></Link>
           </Modal.Footer>
-        </Modal.Dialog>
-      </div>
+      </Modal>
     )
 }
 
