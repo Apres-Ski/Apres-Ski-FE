72d8bb556c4f96ff54622d51064d8d811e23fa91
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-02-10 16:21:38 -0600

    Add filtering logic

--- src/features/restaurants/RestaurantList.tsx
+++ src/features/restaurants/RestaurantList.tsx
@@ -2,15 +2,42 @@ import { RestaurantCard } from './RestaurantCard'
 import { useSelector } from 'react-redux'
 import { RootState } from '../../app/store'
 import { ReactElement, useEffect, useState } from 'react'
+import { CleanedRestaurantsState } from '../../utilities/interfaces'
+import { filter } from 'cypress/types/bluebird'
 
 export const RestaurantList = () => {
   const { restaurants } = useSelector((state: RootState) => state.restaurants)
-  const [renderedRestaurants, setRenderedRestaurants] = useState<ReactElement[]>()
+  const { vibes, hasAlcohol, hasHappyHour } = useSelector(
+    (state: RootState) => state.filters
+  )
+  const [renderedRestaurants, setRenderedRestaurants] =
+    useState<ReactElement[]>()
 
   useEffect(() => {
-    let filteredRestaurants = restaurants.map((restaurant) => <RestaurantCard key={restaurant.id} restaurant={restaurant} />) //naming variable filteredRestaurants for now because I assume later we will do filtering here and maybe change the dependancy to the store filters
-    setRenderedRestaurants(filteredRestaurants)
-  }, [restaurants])
+    let filteredRest = restaurants
+    if (hasAlcohol) {
+      filteredRest = filteredRest.filter((restaurant) => restaurant.alcoholic)
+    }
+
+    if (hasHappyHour) {
+      filteredRest = filteredRest.filter((restaurant) => restaurant.happyHours)
+    }
+
+    if (vibes.length > 0) {
+      filteredRest = filteredRest.filter((restaurant) =>
+        vibes.every((vibe) => restaurant.vibes.includes(vibe))
+      )
+    }
+
+    const restaurantCards = filteredRest.map((restaurant) => (
+      <RestaurantCard
+        key={`restaurant ${restaurant.id}`}
+        restaurant={restaurant}
+      />
+    ))
+    console.log(filteredRest)
+    setRenderedRestaurants(restaurantCards)
+  }, [restaurants, hasAlcohol, hasHappyHour, vibes])
 
   return <div>{renderedRestaurants}</div>
 }
