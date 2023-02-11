6af4aa86d5d4f7701ce45ce7afc25b4f1510a252
Matt Walter <matt.walter.tx@outlook.com>
2023-02-11 13:27:51 -0600

    Correct hours display

--- src/features/restaurants/DetailModal.tsx
+++ src/features/restaurants/DetailModal.tsx
@@ -3,21 +3,32 @@ import type { RootState } from "../../app/store"
 import { selectRestaurant } from './restaurantsSlice'
 
 export const DetailModal = () => {
-    const { activeRestaurant } = useSelector((state: RootState) => state.restaurants)
+  const { activeRestaurant } = useSelector((state: RootState) => state.restaurants)
+
+  const showHours = Object.entries(activeRestaurant.hours).map((day) => {
+      return `${day[0][0].toUpperCase()}: ${day[1]}`
+    })
 
   return (
     <div>
-      <img src={activeRestaurant.coverImg} alt={activeRestaurant.alt} />
-      <button>X</button>
+      <img src={activeRestaurant.coverImg} alt={activeRestaurant.alt} className="img-fluid"/>
       <p>{activeRestaurant.name}</p>
       <p>{activeRestaurant.address}</p>
       <p>Food Type: {activeRestaurant.name}</p>
       <p>Distance: Logic Placeholder</p>
       <p>Cost: {activeRestaurant.cost}</p>
       <p>Alcohol: {activeRestaurant.name}</p>
-      {/* <p>Hours: {activeRestaurant.hours}</p>
-      <p>Happy Hours: {activeRestaurant.happyHours}</p> */}
-      {/* <p>Vibes: {activeRestaurant.vibes}</p> */}
+      <div>Hours: 
+        <p>{showHours[0]}</p>
+        <p>{showHours[1]}</p>
+        <p>{showHours[2]}</p>
+        <p>{showHours[3]}</p>
+        <p>{showHours[4]}</p>
+        <p>{showHours[5]}</p>
+        <p>{showHours[6]}</p>
+      </div>
+      <p>Happy Hours: {activeRestaurant.happyHours}</p>
+      <p>Vibes: {activeRestaurant.vibes}</p>
     </div>
   )
 }
