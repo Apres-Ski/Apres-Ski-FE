163935f78e3ccbf231a0e822ac6c3c035125f65a
Matt Walter <matt.walter.tx@outlook.com>
2023-02-11 13:28:57 -0600

    Correct happy hours display

--- src/features/restaurants/DetailModal.tsx
+++ src/features/restaurants/DetailModal.tsx
@@ -6,8 +6,13 @@ export const DetailModal = () => {
   const { activeRestaurant } = useSelector((state: RootState) => state.restaurants)
 
   const showHours = Object.entries(activeRestaurant.hours).map((day) => {
-      return `${day[0][0].toUpperCase()}: ${day[1]}`
-    })
+    return `${day[0][0].toUpperCase()}: ${day[1]}`
+  })
+
+  const showHappyHours = Object.entries(activeRestaurant.happyHours).map((day) => {
+    return `${day[0][0].toUpperCase()}: ${day[1]}`
+  })
+
 
   return (
     <div>
@@ -18,16 +23,26 @@ export const DetailModal = () => {
       <p>Distance: Logic Placeholder</p>
       <p>Cost: {activeRestaurant.cost}</p>
       <p>Alcohol: {activeRestaurant.name}</p>
-      <div>Hours: 
-        <p>{showHours[0]}</p>
-        <p>{showHours[1]}</p>
-        <p>{showHours[2]}</p>
-        <p>{showHours[3]}</p>
-        <p>{showHours[4]}</p>
-        <p>{showHours[5]}</p>
-        <p>{showHours[6]}</p>
+      <div style={{ display: 'flex' , justifyContent: 'space-around'}}>
+        <div>Hours: 
+          <p>{showHours[0]}</p>
+          <p>{showHours[1]}</p>
+          <p>{showHours[2]}</p>
+          <p>{showHours[3]}</p>
+          <p>{showHours[4]}</p>
+          <p>{showHours[5]}</p>
+          <p>{showHours[6]}</p>
+        </div>
+        <div>Happy Hours: {}
+          <p>{showHappyHours[0]}</p>
+          <p>{showHappyHours[1]}</p>
+          <p>{showHappyHours[2]}</p>
+          <p>{showHappyHours[3]}</p>
+          <p>{showHappyHours[4]}</p>
+          <p>{showHappyHours[5]}</p>
+          <p>{showHappyHours[6]}</p>
+        </div>
       </div>
-      <p>Happy Hours: {activeRestaurant.happyHours}</p>
       <p>Vibes: {activeRestaurant.vibes}</p>
     </div>
   )
