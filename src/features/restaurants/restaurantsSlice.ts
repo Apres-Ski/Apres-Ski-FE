66cd9a56fe2706a8cc0ceede387a0cdcdc99e572
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-02-12 15:42:13 -0600

    Fix type errors

--- src/features/restaurants/restaurantsSlice.ts
+++ src/features/restaurants/restaurantsSlice.ts
@@ -1,11 +1,15 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import { CleanedHappyHours, CleanedHours, CleanedRestaurantsState } from '../../utilities/interfaces'
+import {
+  CleanedHappyHours,
+  CleanedHours,
+  CleanedRestaurantsState,
+} from '../../utilities/interfaces'
 import { fetchRestaurants } from '../../utilities/APICalls'
 import { cleanList } from '../../utilities/utilities'
 
 export interface RestaurantsState {
   restaurants: CleanedRestaurantsState[]
-  activeRestaurant: CleanedRestaurantsState 
+  activeRestaurant: CleanedRestaurantsState
   filteredRestaurants: CleanedRestaurantsState[]
   status: string
   error: string | null
@@ -17,7 +21,7 @@ const initialState: RestaurantsState = {
   status: 'idle',
   error: null,
   activeRestaurant: {
-    id: null,
+    id: '',
     name: '',
     address: '',
     foodType: '',
@@ -27,22 +31,25 @@ const initialState: RestaurantsState = {
     alt: '',
     location: {
       lat: '',
-      long: ''
+      long: '',
     },
     alcoholic: false,
     happyHour: false,
     hours: {} as CleanedHours,
     happyHours: {} as CleanedHappyHours,
     engagements: [],
-    avgRating: 0
-  }
+    avgRating: 0,
+  },
 }
 
-export const getRestaurants = createAsyncThunk('restaurants/fetchRestaurants', async () => {
-  const response = await fetchRestaurants()
-  const restaurantList = cleanList(response.data)
-  return restaurantList
-})
+export const getRestaurants = createAsyncThunk(
+  'restaurants/fetchRestaurants',
+  async () => {
+    const response = await fetchRestaurants()
+    const restaurantList = cleanList(response.data)
+    return restaurantList
+  }
+)
 
 export const restaurantsSlice = createSlice({
   name: 'restaurants',
@@ -52,29 +59,29 @@ export const restaurantsSlice = createSlice({
       state.filteredRestaurants = action.payload
     },
     selectRestaurant(state, action) {
-      const restaurant = action.payload;
-      (state.activeRestaurant = restaurant)
+      const restaurant = action.payload
+      state.activeRestaurant = restaurant
     },
-    resetRestaurant (state, action) {
-      state.activeRestaurant.id = null,
-      state.activeRestaurant.name = '',
-      state.activeRestaurant.address = '',
-      state.activeRestaurant.foodType = '',
-      state.activeRestaurant.cost = '',
-      state.activeRestaurant.vibes = [],
-      state.activeRestaurant.coverImg = '',
-      state.activeRestaurant.alt = '',
-      state.activeRestaurant.location = {
-        lat: '',
-        long: ''
-      },
-      state.activeRestaurant.alcoholic = false,
-      state.activeRestaurant.happyHour = false,
-      state.activeRestaurant.hours = {} as CleanedHours,
-      state.activeRestaurant.happyHours = {} as CleanedHappyHours,
-      state.activeRestaurant.engagements = [],
-      state.activeRestaurant.avgRating = 0
-    }
+    resetRestaurant(state, action) {
+      ;(state.activeRestaurant.id = ''),
+        (state.activeRestaurant.name = ''),
+        (state.activeRestaurant.address = ''),
+        (state.activeRestaurant.foodType = ''),
+        (state.activeRestaurant.cost = ''),
+        (state.activeRestaurant.vibes = []),
+        (state.activeRestaurant.coverImg = ''),
+        (state.activeRestaurant.alt = ''),
+        (state.activeRestaurant.location = {
+          lat: '',
+          long: '',
+        }),
+        (state.activeRestaurant.alcoholic = false),
+        (state.activeRestaurant.happyHour = false),
+        (state.activeRestaurant.hours = {} as CleanedHours),
+        (state.activeRestaurant.happyHours = {} as CleanedHappyHours),
+        (state.activeRestaurant.engagements = []),
+        (state.activeRestaurant.avgRating = 0)
+    },
   },
   extraReducers(builder) {
     builder
@@ -93,6 +100,7 @@ export const restaurantsSlice = createSlice({
   },
 })
 
-export const { filterRestaurants, selectRestaurant, resetRestaurant } = restaurantsSlice.actions
+export const { filterRestaurants, selectRestaurant, resetRestaurant } =
+  restaurantsSlice.actions
 
 export default restaurantsSlice.reducer
