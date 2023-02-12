7c27b184d03254d9f8b25eee23a45f88bc369302
Matt Walter <matt.walter.tx@outlook.com>
2023-02-11 18:44:09 -0600

    Correct formatting

--- src/features/restaurants/DetailModal.tsx
+++ src/features/restaurants/DetailModal.tsx
@@ -1,16 +1,18 @@
-import { useSelector, useDispatch } from "react-redux"
-import type { RootState } from "../../app/store"
-import { HappyHours } from "./HappyHours"
+import { useSelector } from 'react-redux'
+import type { RootState } from '../../app/store'
+import { HappyHours } from './HappyHours'
 
 export const DetailModal = () => {
-  const { activeRestaurant } = useSelector((state: RootState) => state.restaurants)
+  const { activeRestaurant } = useSelector(
+    (state: RootState) => state.restaurants
+  )
 
   const showHours = Object.entries(activeRestaurant.hours).map((day) => {
     return `${day[0][0].toUpperCase()}: ${day[1]}`
   })
 
-  function hasBooze () {
-    if(activeRestaurant.alcoholic) {
+  function hasBooze() {
+    if (activeRestaurant.alcoholic) {
       return 'Yes'
     } else {
       return 'No'
@@ -19,15 +21,20 @@ export const DetailModal = () => {
 
   return (
     <div>
-      <img src={activeRestaurant.coverImg} alt={activeRestaurant.alt} className="img-fluid"/>
+      <img
+        src={activeRestaurant.coverImg}
+        alt={activeRestaurant.alt}
+        className="img-fluid"
+      />
       <p>{activeRestaurant.name}</p>
       <p>{activeRestaurant.address}</p>
       <p>Food Type: {activeRestaurant.foodType}</p>
       <p>Distance: Logic Placeholder</p>
       <p>Cost: {activeRestaurant.cost}</p>
       <p>Alcohol: {hasBooze()}</p>
-      <div style={{ display: 'flex' , justifyContent: 'space-around'}}>
-        <div>Hours: 
+      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+        <div>
+          Hours:
           <p>{showHours[0]}</p>
           <p>{showHours[1]}</p>
           <p>{showHours[2]}</p>
@@ -36,7 +43,11 @@ export const DetailModal = () => {
           <p>{showHours[5]}</p>
           <p>{showHours[6]}</p>
         </div>
-        {activeRestaurant.happyHours ? <HappyHours /> : <p>No Specials Available</p>}
+        {activeRestaurant.happyHours ? (
+          <HappyHours />
+        ) : (
+          <p>No Specials Available</p>
+        )}
       </div>
       <p>Vibes: {activeRestaurant.vibes}</p>
     </div>
--- src/features/restaurants/HappyHours.tsx
+++ src/features/restaurants/HappyHours.tsx
@@ -1,23 +1,28 @@
-import { useSelector } from "react-redux"
-import type { RootState } from "../../app/store"
+import { useSelector } from 'react-redux'
+import type { RootState } from '../../app/store'
 
 export const HappyHours = () => {
-  const { activeRestaurant } = useSelector((state: RootState) => state.restaurants)
+  const { activeRestaurant } = useSelector(
+    (state: RootState) => state.restaurants
+  )
 
   function showHappyHours() {
     return Object.entries(activeRestaurant.happyHours).map((day) => {
-      if(day[1]) {
+      if (day[1]) {
         return `${day[0][0].toUpperCase()}: ${day[1]}`
-  }})!
-}
+      }
+    })!
+  }
 
-  function showSpecial () {
+  function showSpecial() {
     return Object.entries(activeRestaurant.happyHours).find((item) => {
       return item[0] === 'special'
-    })!}
+    })!
+  }
 
   return (
-      <div>Happy Hours: {showSpecial()[1]}
+    <div>
+      Happy Hours: {showSpecial()[1]}
       <p>{showHappyHours()[0]}</p>
       <p>{showHappyHours()[1]}</p>
       <p>{showHappyHours()[2]}</p>
