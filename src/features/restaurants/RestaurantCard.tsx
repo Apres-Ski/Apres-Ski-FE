5b7837fe360eff8a5035df7f9fd3637043e68682
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-02-13 16:28:19 -0600

    Add initial card container

--- src/features/restaurants/RestaurantCard.tsx
+++ src/features/restaurants/RestaurantCard.tsx
@@ -7,12 +7,16 @@ import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
 import { useState } from 'react'
 
-export const RestaurantCard = ({ restaurant }: { restaurant: CleanedRestaurantsState }) => {
+export const RestaurantCard = ({
+  restaurant,
+}: {
+  restaurant: CleanedRestaurantsState
+}) => {
   const dispatch = useDispatch<AppDispatch>()
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false)
   const formattedVibes = restaurant.vibes.join(', ')
 
-  const handleClose = () => setShow(false);
+  const handleClose = () => setShow(false)
 
   const handleClick = () => {
     dispatch(selectRestaurant(restaurant))
@@ -20,30 +24,42 @@ export const RestaurantCard = ({ restaurant }: { restaurant: CleanedRestaurantsS
   }
 
   return (
-    <div>
-      <p>{restaurant.name}</p>
-      <p>Vibe: {formattedVibes}</p>
-      <p>Distance: .25m</p>
-      <img style={{ width: 350 }} src={restaurant.coverImg} />
-      <p>PLACEHOLDER FOR VIBE BADGES</p>
-      <button onClick={() => handleClick()} type="button" className="btn btn-primary" data-toggle="modal" data-target={restaurant.id}>
-        Show Details
-      </button>
-
-      <Modal
-        show={show}
-        onHide={handleClose}
-        backdrop="static"
-      >
-        <Modal.Header className='d-flex justify-content-center'>
-        </Modal.Header>
-          <Modal.Body className='d-flex justify-content-center'>
-            {<DetailModal />}
-          </Modal.Body>
-        <Modal.Footer className='d-flex justify-content-center'>
-          <Button onClick={() => handleClose()}variant='primary'>Close</Button>
-        </Modal.Footer>
-      </Modal>
+    <div className="m-4">
+      <div className="card" style={{ maxWidth: '500' }}>
+        <div className="row g-0">
+          <div className="col-sm-5">
+            <img className="card-img-top h-100" src={restaurant.coverImg} />
+          </div>
+          <div className="col-sm-7">
+            <div className="card-body">
+              <p>{restaurant.name}</p>
+              <p>Vibe: {formattedVibes}</p>
+              <p>Distance: .25m</p>
+              <p>PLACEHOLDER FOR VIBE BADGES</p>
+              <button
+                onClick={() => handleClick()}
+                type="button"
+                className="btn btn-primary"
+                data-toggle="modal"
+                data-target={restaurant.id}
+              >
+                Show Details
+              </button>
+              <Modal show={show} onHide={handleClose} backdrop="static">
+                <Modal.Header className="d-flex justify-content-center"></Modal.Header>
+                <Modal.Body className="d-flex justify-content-center">
+                  {<DetailModal />}
+                </Modal.Body>
+                <Modal.Footer className="d-flex justify-content-center">
+                  <Button onClick={() => handleClose()} variant="primary">
+                    Close
+                  </Button>
+                </Modal.Footer>
+              </Modal>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
