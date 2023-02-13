8fde14797141463b97def08794c017a74d1755af
Matt Walter <matt.walter.tx@outlook.com>
2023-02-13 11:05:25 -0600

    Fill out landing test

--- cypress/e2e/landing.cy.ts
+++ cypress/e2e/landing.cy.ts
@@ -1,5 +1,24 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5173/')
+  })
+
+  it('should find the landing content', () => {
+    cy.get('p').should('exist')
+  })
+
+  it('should find the landing content', () => {
+    cy.get('.btn').should('exist')
+  })
+
+  it('should be able to click through to the users page', () => {
+    cy.get('.btn').click().url().should('eq', 'http://127.0.0.1:5173/demo')
+  })
+
+  it('should be able to use browser arrows to return to landing', () => {
+    cy.get('.btn').click()
+      .url().should('eq', 'http://127.0.0.1:5173/demo')
+      .go('back').url().should('eq', 'http://127.0.0.1:5173/')
+      .go('forward').url().should('eq', 'http://127.0.0.1:5173/demo')
   })
 })
