1b199e13ecdfa476cbcaa2ffa350833cd0944209
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-02-13 16:58:03 -0600

    Update img size

--- src/features/restaurants/RestaurantCard.tsx
+++ src/features/restaurants/RestaurantCard.tsx
@@ -24,27 +24,33 @@ export const RestaurantCard = ({
   }
 
   return (
-    <div className="m-4">
-      <div className="card" style={{ maxWidth: '500' }}>
-        <div className="row g-0">
-          <div className="col-sm-5">
-            <img className="card-img-top h-100" src={restaurant.coverImg} />
-          </div>
-          <div className="col-sm-7">
-            <div className="card-body">
-              <p>{restaurant.name}</p>
-              <p>Vibe: {formattedVibes}</p>
-              <p>Distance: .25m</p>
-              <p>PLACEHOLDER FOR VIBE BADGES</p>
+    <div className="row g-5 mt-1 justify-content-evenly">
+      <div className="col-lg-6">
+        <div className="card">
+          <div className="row g-0">
+            <div className="col-6 col-md-5">
+              <img className="card-img" src={restaurant.coverImg} />
+            </div>
+            <div className="col-6 col-md-7">
+              <div className="card-body d-flex flex-column">
+                <div className="h-100">
+                  <h3 className="card-title">{restaurant.name}</h3>
+                  <p className="card-text">Vibe: {formattedVibes}</p>
+                  <p className="card-text">Distance: .25m</p>
+                  <p className="card-text">PLACEHOLDER FOR VIBE BADGES</p>
+                </div>
+              </div>
+              <div>
               <button
                 onClick={() => handleClick()}
                 type="button"
-                className="btn btn-primary"
+                className="btn btn-dark m-2"
                 data-toggle="modal"
                 data-target={restaurant.id}
               >
                 Show Details
               </button>
+              </div>
               <Modal show={show} onHide={handleClose} backdrop="static">
                 <Modal.Header className="d-flex justify-content-center"></Modal.Header>
                 <Modal.Body className="d-flex justify-content-center">
