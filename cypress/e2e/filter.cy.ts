9c04bc540e3dbbf63389c9f3bb91ff9bd6ff2954
Matt Walter <matt.walter.tx@outlook.com>
2023-02-14 15:24:34 -0600

    Add test for empty list sorry comp

--- cypress/e2e/filter.cy.ts
+++ cypress/e2e/filter.cy.ts
@@ -64,6 +64,14 @@ describe('Filter Test', () => {
     cy.get('#card-3 > :nth-child(1)').contains('Cabin Juice')
   })
 
+  it('should be able to select other options and filter the list', () => {
+    cy.get('[for="fun"]').click()
+    cy.get(':nth-child(20) > .form-switch > #custom-switch').check()
+    cy.get('.btn-close').click({ force: false })
+    cy.get(':nth-child(5) > div').should('exist')
+    cy.get('#card-2 > :nth-child(1)').contains('BoLD Restaurant and Bar')
+  })
+
   it('should be able to reset the filters', () => {
     cy.get('[for="cozy"]').click()
     cy.get(':nth-child(19) > .form-switch > #custom-switch').check()
@@ -83,10 +91,34 @@ describe('Filter Test', () => {
     cy.get('#card-3 > :nth-child(1)').contains('Cabin Juice')
   })
 
-  it('should be able to select other options and filter the list', () => {
-    cy.get('[for="fun"]').click()
+
+  it('should find an error message on the dashboard when no results match', () => {
+    cy.get('[for="cozy"]').click()
+    cy.get('[for="quickEasy"]').click()
     cy.get('.btn-close').click({ force: false })
-    cy.get(':nth-child(5) > div').should('exist')
-    cy.get('#card-2 > :nth-child(1)').contains('BoLD Restaurant and Bar')
+    cy.get('.fade-in > :nth-child(4) > div').should('exist')
+    cy.get('.fade-in > :nth-child(4) > div').contains(
+      'No matches for this criteria. Please refine your selections and try again.'
+    )
+ })
+
+  it('should be able to use button to change filter options', () => {
+    cy.get('[for="cozy"]').click()
+    cy.get('[for="quickEasy"]').click()
+    cy.get('.btn-close').click({ force: false })
+    cy.get('.fade-in > :nth-child(4) > div > :nth-child(2)').click()
+  })
+
+  it('should be able to use the button to reset all filter options and re-render all cards', () => {
+    cy.get('[for="cozy"]').click()
+    cy.get('[for="quickEasy"]').click()
+    cy.get('.btn-close').click({ force: false })
+    cy.get('.fade-in > :nth-child(4) > div').contains(
+      'No matches for this criteria. Please refine your selections and try again.'
+    )
+    cy.get(':nth-child(4) > div > :nth-child(3)').click()
+    cy.get('#card-2 > :nth-child(1) > .col-md-8 > .card-body').should('exist')
+    cy.get('#card-3 > :nth-child(1) > .col-md-8 > .card-body').should('exist')
+    cy.get('#card-1 > :nth-child(1) > .col-md-8 > .card-body').should('exist')
   })
 })
