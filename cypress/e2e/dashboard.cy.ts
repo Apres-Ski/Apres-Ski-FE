ecbf47b30bb463c7d9c3d671df81fbfaa1793d17
Matt Walter <matt.walter.tx@outlook.com>
2023-02-14 17:03:49 -0600

    Update elements and formatting

--- cypress/e2e/dashboard.cy.ts
+++ cypress/e2e/dashboard.cy.ts
@@ -19,7 +19,7 @@ describe('Dashboard Test', () => {
       }
     )
     cy.intercept(
-        'GET', 
+      'GET',
       'https://arcane-inlet-03546.herokuapp.com/api/v1/lift/',
       {
         statusCode: 200,
@@ -34,9 +34,11 @@ describe('Dashboard Test', () => {
       .should('eq', 'https://apres-ski-fe.vercel.app/dashboard')
   })
 
-  it('should find a Nav bar with the selected User\s name', () => {
-    cy.get('.navbar').should('exist')
-    cy.get('.navbar-nav > :nth-child(1)').contains('Lily White')
+  it('should find a Nav bar with the selected Users name', () => {
+    cy.get('.navbar')
+      .should('exist')
+    cy.get('.navbar-nav > :nth-child(1)')
+      .contains('Lily White')
   })
 
   it('should click the app logo to return to start', () => {
@@ -56,61 +58,76 @@ describe('Dashboard Test', () => {
   })
 
   it('should find the Map', () => {
-    cy.get(':nth-child(5) > .mapboxgl-canvas').should('exist')
+    cy.get(':nth-child(5) > .mapboxgl-canvas')
+      .should('exist')
   })
 
   it('Should find a Filter button that opens menu when clicked', () => {
-    cy.get('button').contains('Filter').should('exist').click({ force: true })
-    cy.get('.offcanvas-body').should('exist')
+    cy.get('button')
+      .contains('Filter')
+      .should('exist')
+      .click({ force: true })
+    cy.get('.offcanvas-body')
+      .should('exist')
   })
 
-  it.skip('should find 3 restaurant cards in order by distance', () => {
-    cy.get('#card-2').should('exist')
-    // cy.get('#card-2 > :nth-child(3)')
-    //   .contains('0.01 miles')
-    cy.get('#card-3').should('exist')
-    // cy.get('#card-3 > :nth-child(3)')
-    //   .contains('0.06 miles')
-    cy.get('#card-1').should('exist')
-    // cy.get('#card-1 > :nth-child(3)')
-    //   .contains('0.27 miles')
+  it('should find 3 restaurant cards in order by distance', () => {
+    cy.get('#card-2')
+      .should('exist')
+    cy.get('#card-2 > :nth-child(1) > .col-md-8 > .card-body > .row > :nth-child(1) > .card-text.small-text > span')
+      .contains('0.01 miles')
+    cy.get('#card-3')
+      .should('exist')
+    cy.get('#card-3 > :nth-child(1) > .col-md-8 > .card-body > .row > :nth-child(1) > .card-text.small-text > span')
+      .contains('0.06 miles')
+    cy.get('#card-1')
+      .should('exist')
+    cy.get('#card-1 > :nth-child(1) > .col-md-8 > .card-body > .row > :nth-child(1) > .card-text.small-text > span')
+      .contains('0.27 miles')
   })
 
   it('should find the initial details for a restaurant on its card', () => {
-    cy.get('#card-1').contains('9600 Kitchen')
-    // cy.get('#card-1 > :nth-child(2)').contains('casual, upscale')
-    // cy.get('#card-1 > :nth-child(3)').contains('0.27 miles')
-    cy.get('#card-2 > :nth-child(1) > .col-md-4 > .card-img').should('exist')
+    cy.get('#card-1')
+      .contains('9600 Kitchen')
+    cy.get('#card-1 > :nth-child(1) > .col-md-8 > .card-body > .row > :nth-child(1) > .card-text.small-text > span')
+      .contains('0.27 miles')
+    cy.get('#card-2 > :nth-child(1) > .col-md-4 > .card-img')
+      .should('exist')
     cy.get('#card-1 > :nth-child(1) > .col-md-8 > .card-body > .row > .label-container > .blue')
       .should('exist')
     cy.get('#card-1 > :nth-child(1) > .col-md-8 > .card-body > .row > .label-container > .blue')
       .should('exist')
   })
-  
+
   it('should find a button to click for more details', () => {
     cy.get('#card-1 > :nth-child(1) > .col-md-8 > .card-body > .btn')
       .should('exist')
       .click({ force: true })
-    cy.get('.img-fluid').should('exist')
-    cy.get('.modal-body > :nth-child(1) > :nth-child(2)')
+    cy.get('.img-fluid')
+      .should('exist')
+    cy.get('[style="text-align: center;"] > .mb-3')
       .contains('9600 Kitchen')
-    cy.get('.modal-body > :nth-child(1) > :nth-child(3)')
+    cy.get('[style="text-align: center;"] > :nth-child(2)')
       .contains('550 Village Rd Breckenridge, CO 80424')
-    cy.get('.modal-body > :nth-child(1) > :nth-child(4)')
+    cy.get('.d-flex > :nth-child(1) > [style="letter-spacing: 0.3rem;"]')
       .contains('American')
-    cy.get('.modal-body > :nth-child(1) > :nth-child(5)')
-      .contains('Logic Placeholder')
-    cy.get('.modal-body > :nth-child(1) > :nth-child(4)').should('exist')
-    cy.get('[style="display: flex; justify-content: space-around;"] > :nth-child(2)')
+    cy.get('[style="margin: 1rem;"] > .card-text > span')
+      .contains('0.27 miles')
+    cy.get('.modal-body > :nth-child(1) > :nth-child(4)')
+      .should('exist')
+    cy.get('[style="text-align: center;"] > .m-4')
       .contains('No Specials Available')
-    cy.get('.modal-body > :nth-child(1) > :nth-child(9)').should('exist')
-    cy.get('.modal-footer > .btn').should('exist')
+    cy.get('.modal-body > :nth-child(1) > :nth-child(9)')
+      .should('exist')
+    cy.get('.modal-footer > .btn')
+      .should('exist')
   })
 
   it('should click the button to close the modal', () => {
     cy.get('#card-1 > :nth-child(1) > .col-md-8 > .card-body > .btn')
       .should('exist')
       .click({ force: true })
-    cy.get('.modal-footer > .btn').click()
+    cy.get('.modal-footer > .btn')
+      .click()
   })
 })
