bcf0865c307d309020210ba4d0e747ea4b0385e0
Matt Walter <matt.walter.tx@outlook.com>
2023-02-14 09:57:33 -0600

    Update dashboard paths/ elements

--- cypress/e2e/dashboard.cy.ts
+++ cypress/e2e/dashboard.cy.ts
@@ -1,15 +1,32 @@
 describe('Dashboard Test', () => {
   beforeEach(() => {
-    cy.intercept('GET', 'http://127.0.0.1:8000/api/v1/user/', {
-      statusCode: 200,
-      ok: true,
-      fixture: 'users',
-    })
-    cy.intercept("GET", 'http://127.0.0.1:8000/api/v1/restaurant/', {
-      statusCode: 200,
-      ok: true,
-      fixture: 'restaurants',
-    })
+    cy.intercept(
+      'GET',
+      'https://arcane-inlet-03546.herokuapp.com/api/v1/user/',
+      {
+        statusCode: 200,
+        ok: true,
+        fixture: 'users',
+      }
+    )
+    cy.intercept(
+      'GET',
+      'https://arcane-inlet-03546.herokuapp.com/api/v1/restaurant/',
+      {
+        statusCode: 200,
+        ok: true,
+        fixture: 'restaurants',
+      }
+    )
+    cy.intercept(
+        'GET', 
+      'https://arcane-inlet-03546.herokuapp.com/api/v1/lift/',
+      {
+        statusCode: 200,
+        ok: true,
+        fixture: 'lifts',
+      }
+    )
     cy.visit('http://127.0.0.1:5173/demo')
     cy.get(':nth-child(1) > a > .img-fluid').click()
       .url().should('eq', 'http://127.0.0.1:5173/dashboard')
@@ -31,7 +48,7 @@ describe('Dashboard Test', () => {
   })
 
   it('should find the Map', () => {
-    cy.get(':nth-child(9) > .mapboxgl-canvas').should('exist')
+    cy.get(':nth-child(14) > .mapboxgl-canvas').should('exist')
   })
 
   it('Should find a Filter button that opens menu when clicked', () => {
@@ -39,27 +56,28 @@ describe('Dashboard Test', () => {
     cy.get('.offcanvas-body').should('exist')
   })
 
-  it('should find 3 restaurant cards', () => {
-    cy.get(':nth-child(1) > :nth-child(5) > :nth-child(1)').should('exist')
-    cy.get(':nth-child(1) > :nth-child(5) > :nth-child(2)').should('exist')
-    cy.get(':nth-child(1) > :nth-child(5) > :nth-child(3)').should('exist')
+  it('should find 3 restaurant cards in order by distance', () => {
+    cy.get('#card-2').should('exist')
+    cy.get('#card-2 > :nth-child(3)')
+      .contains('0.01 miles')
+    cy.get('#card-3').should('exist')
+    cy.get('#card-3 > :nth-child(3)')
+      .contains('0.06 miles')
+    cy.get('#card-1').should('exist')
+    cy.get('#card-1 > :nth-child(3)')
+      .contains('0.27 miles')
   })
 
   it('should find the initial details for a restaurant on its card', () => {
-    cy.get(':nth-child(1) > :nth-child(5) > :nth-child(1) > :nth-child(1)')
-      .contains('9600 Kitchen')
-    cy.get(':nth-child(1) > :nth-child(5) > :nth-child(1) > :nth-child(2)')
-      .contains('casual, upscale')
-    cy.get(':nth-child(1) > :nth-child(5) > :nth-child(1) > :nth-child(3)')
-      .contains('.25m')
-    cy.get(':nth-child(1) > img').should('exist')
-    cy.get(':nth-child(1) > :nth-child(5) > :nth-child(1) > :nth-child(5)')
-      .contains('PLACEHOLDER FOR VIBE BADGES')
+    cy.get('#card-1').contains('9600 Kitchen')
+    cy.get('#card-1 > :nth-child(2)').contains('casual, upscale')
+    cy.get('#card-1 > :nth-child(3)').contains('0.27 miles')
+    cy.get('#card-1 > img').should('exist')
+    cy.get('#card-1 > :nth-child(5)').contains('PLACEHOLDER FOR VIBE BADGES')
   })
   
   it('should find a button to click for more details', () => {
-    cy.get(':nth-child(5) > :nth-child(1) > .btn').should('exist')
-      .click({ force: true })
+    cy.get('#card-1 > .btn').should('exist').click({ force: true })
     cy.get('.img-fluid').should('exist')
     cy.get('.modal-body > :nth-child(1) > :nth-child(2)')
       .contains('9600 Kitchen')
@@ -77,8 +95,7 @@ describe('Dashboard Test', () => {
   })
 
   it('should click the button to close the modal', () => {
-    cy.get(':nth-child(5) > :nth-child(1) > .btn').should('exist')
-      .click({ force: true })
+    cy.get('#card-1 > .btn').should('exist').click({ force: true })
     cy.get('.modal-footer > .btn').click()
   })
 })
