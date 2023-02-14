5270ed235d22940959fe02141eeab7bf320722a7
Matt Walter <matt.walter.tx@outlook.com>
2023-02-13 21:03:38 -0600

    Fill out filter tests

--- cypress/e2e/filter.cy.ts
+++ cypress/e2e/filter.cy.ts
@@ -5,16 +5,17 @@ describe('Filter Test', () => {
       ok: true,
       fixture: 'users',
     })
-    cy.intercept("GET", 'http://127.0.0.1:8000/api/v1/restaurant/', {
+    cy.intercept('GET', 'http://127.0.0.1:8000/api/v1/restaurant/', {
       statusCode: 200,
       ok: true,
       fixture: 'restaurants',
     })
     cy.visit('http://127.0.0.1:5173/demo')
-    cy.get(':nth-child(1) > a > .img-fluid').click()
-      .url().should('eq', 'http://127.0.0.1:5173/dashboard')
+    cy.get(':nth-child(1) > a > .img-fluid')
+      .click()
+      .url()
+      .should('eq', 'http://127.0.0.1:5173/dashboard')
     cy.get('button').contains('Filter').should('exist').click({ force: true })
-
   })
 
   it('should find the filter header with a closing button and title', () => {
@@ -37,8 +38,42 @@ describe('Filter Test', () => {
     cy.get('[for="relaxed"]').should('exist')
   })
 
-  it.only('should find a toggle for alcohol and for happy hours', () => {
+  it('should find a toggle for alcohol and for happy hours', () => {
     cy.get(':nth-child(19) > .form-switch > #custom-switch').should('exist')
     cy.get(':nth-child(20) > .form-switch > #custom-switch').should('exist')
+  })
+
+  it('should be able to select options and filter the list', () => {
+    cy.get('[for="cozy"]').click()
+    cy.get(':nth-child(19) > .form-switch > #custom-switch').check()
+    cy.get('.btn-close').click({ force: false })
+    cy.get(':nth-child(5) > div').should('exist')
+    cy.get(':nth-child(5) > div > :nth-child(1)').contains('Cabin Juice')
+  })
+
+  it('should be able to reset the filters', () => {
+    cy.get('[for="cozy"]').click()
+    cy.get(':nth-child(19) > .form-switch > #custom-switch').check()
+    cy.get('.btn-close').click({ force: false })
+
+    cy.get(':nth-child(5) > div').should('exist')
+    cy.get(':nth-child(5) > div > :nth-child(1)').contains('Cabin Juice')
+    cy.get('button').contains('Filter').should('exist').click({ force: true })
+    cy.get('.btn-outline-success').click()
+    cy.get('.btn-close').click({ force: false })
+
+    cy.get(':nth-child(5) > :nth-child(1)').should('exist')
+    cy.get(':nth-child(1) > :nth-child(5) > :nth-child(1) > :nth-child(1)').contains('9600 Kitchen')
+    cy.get(':nth-child(5) > :nth-child(2)').should('exist')
+    cy.get(':nth-child(5) > div > :nth-child(1)').contains('BoLD Restaurant and Bar')
+    cy.get(':nth-child(5) > :nth-child(3)').should('exist')
+    cy.get(':nth-child(5) > div > :nth-child(1)').contains('Cabin Juice')
+  })
+
+  it('should be able to select options and filter the list', () => {
+    cy.get('[for="fun"]').click()
+    cy.get('.btn-close').click({ force: false })
+    cy.get(':nth-child(5) > div').should('exist')
+    cy.get(':nth-child(5) > div > :nth-child(1)').contains('BoLD Restaurant and Bar')
   })
 })
