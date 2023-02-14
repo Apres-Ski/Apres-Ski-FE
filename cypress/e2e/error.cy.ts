52092f2431d3ae3f84eab5014f71a111f4ed2ad2
Matt Walter <matt.walter.tx@outlook.com>
2023-02-14 17:04:30 -0600

    Update formatting

--- cypress/e2e/error.cy.ts
+++ cypress/e2e/error.cy.ts
@@ -1,13 +1,16 @@
-describe('template spec', () => {
+describe('Error Test', () => {
   beforeEach(() => {
     cy.visit('https://apres-ski-fe.vercel.app/')
     cy.visit('https://apres-ski-fe.vercel.app/sdojbsdv')
   })
 
   it('should load error content with bad path', () => {
-    cy.get('.modal-header').should('exist')
-    cy.get('.modal-body').should('exist')
-    cy.get('.btn').should('exist')
+    cy.get('.modal-header')
+      .should('exist')
+    cy.get('.modal-body')
+      .should('exist')
+    cy.get('.btn')
+      .should('exist')
   })
 
   it('should click the button to close and return to landing', () => {
