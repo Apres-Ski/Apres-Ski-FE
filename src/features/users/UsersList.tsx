891cac5c9f16c52a5dc777e8625a149c4dd9a281
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-02-14 18:34:38 -0600

    Fix user cards and background

--- src/features/users/UsersList.tsx
+++ src/features/users/UsersList.tsx
@@ -7,6 +7,8 @@ import { resetFilters } from '../filters/filtersSlice'
 import user1 from '../../assets/user1.jpg'
 import user2 from '../../assets/user2.jpg'
 import user3 from '../../assets/user3.jpg'
+import lilyWhite from '../../assets/lily.jpg'
+import { Card, Col, Figure, Row } from 'react-bootstrap'
 
 const colors = ['danger', 'green-accent', 'success']
 const usersList = [user1, user2, user3]
@@ -20,30 +22,52 @@ const UsersList = () => {
   }, [activeUser])
 
   const renderedUsers = users.map((user, index) => {
-    
-    return <div key={user.id} className='container mt-4' style={{ width: '45%' }}>
-      <Link to="/dashboard">
-        <img
-          src={usersList[index]}
-          className={`img-fluid rounded border border-2 border-white`}
-          alt='...'
-          onClick={() => dispatch(selectUser(user))}
-        />
-      </Link>
-      <p className={`text-${colors[index]}`}>{user.name}</p>
-    </div>
-} )
+    return (
+      <div key={user.id} className="container mt-4" style={{ width: '45%' }}>
+        <Link to="/dashboard">
+          <img
+            src={usersList[index]}
+            className={`img-fluid rounded border border-2 border-white`}
+            alt="..."
+            onClick={() => dispatch(selectUser(user))}
+          />
+        </Link>
+        <p className={`text-${colors[index]}`}>{user.name}</p>
+      </div>
+    )
+  })
 
   return (
-    <div>
-      <section className='d-flex'>
-        {renderedUsers[0]}
-        {renderedUsers[1]}
-      </section>
-      <section>
-        {renderedUsers[2]}
-      </section>
+    <div className='d-flex m-4 align-items-center justify-content-center flex-wrap'>
+    <Card style={{width: '24rem', margin: '2rem'}}>
+      <Card.Img variant="top" src={lilyWhite}/>
+      <Card.Body>
+        <Card.Title>{users[0].name}</Card.Title>
+      </Card.Body>
+    </Card>
+    <Card style={{width: '24rem', margin: '2rem'}}>
+      <Card.Img variant="top" src={lilyWhite}/>
+      <Card.Body>
+        <Card.Title>{users[0].name}</Card.Title>
+      </Card.Body>
+    </Card>
+    <Card style={{width: '24rem', margin: '2rem'}}>
+      <Card.Img variant="top" src={lilyWhite}/>
+      <Card.Body>
+        <Card.Title>{users[0].name}</Card.Title>
+      </Card.Body>
+    </Card>
+    {/* // <div>
+    //   <section className='d-flex'>
+    //     {renderedUsers[0]}
+    //     {renderedUsers[1]}
+    //   </section>
+    //   <section>
+    //     {renderedUsers[2]}
+    //   </section>
+    // </div> */}
     </div>
-)}
+  )
+}
 
 export default UsersList
